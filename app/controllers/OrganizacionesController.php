5022e35ffb7ce2eff36fe72d3fd7834af891eecf
Mayanin <mayanin.sobrevilla@gmail.com>
2015-06-07 15:31:10 -0500

    desarrollo de organizaciones

--- app/controllers/OrganizacionesController.php
+++ app/controllers/OrganizacionesController.php
@@ -9,7 +9,7 @@ class OrganizacionesController extends \BaseController {
 	 */
 	public function index()
 	{
-		$organizaciones = Organizacion::all();
+		$organizaciones = Organizacion::orderBy('id_organizacion', 'desc')->get();
 		return View::make('organizaciones.index', compact('organizaciones'));
 	}
 
@@ -40,7 +40,7 @@ class OrganizacionesController extends \BaseController {
             'contacto' => 'required|alpha_space|between:1,255',
             'telefono' => 'required|required|regex:/^[0-9]{10,20}$/',
             'correo' => 'required|email',
-            'estado' => 'required|in:Activo,Inactivo',
+            'estado' => 'required|in:Activo,Inactivo'
         ];
 
         $messages = [
@@ -52,7 +52,7 @@ class OrganizacionesController extends \BaseController {
             'between' => 'Este campo es obligatorio.',
             'telefono.regex' => 'El formato ingresado no es válido',
             'email' => 'El correo debe estar formado de la siguiente manera: direccion@dominio.com',
-            'in' => 'Este campo es obligatorio.',
+            'in' => 'Este campo es obligatorio.'
         ];
 
         $validator = Validator::make($data = Input::all(), $rules, $messages);
@@ -66,34 +66,34 @@ class OrganizacionesController extends \BaseController {
         $data['creacion'] = date('Y-m-d H:i:s');
         Dependencia::create($data);
 
-		return Redirect::route('dependencias.index')->with('message-type', 'success')
+		return Redirect::route('organizaciones.index')->with('message-type', 'success')
             ->with('message', 'La información se ha guardado correctamente');
 	}
 
 	/**
-	 * Display the specified dependencia.
+	 * Display the specified organizacion.
 	 *
 	 * @param  int  $id
 	 * @return Response
 	 */
 	public function show($id)
 	{
-		$dependencia = Dependencia::findOrFail($id);
+		$organizacion = Dependencia::findOrFail($id);
 
-		return View::make('dependencias.show', compact('dependencia'));
+		return View::make('organizaciones.show', compact('organizacion'));
 	}
 
 	/**
-	 * Show the form for editing the specified dependencia.
+	 * Show the form for editing the specified organizacion.
 	 *
 	 * @param  int  $id
 	 * @return Response
 	 */
 	public function edit($id)
 	{
-		$dependencia = Dependencia::find($id);
+		$organizacion = Dependencia::find($id);
 
-		return View::make('dependencias.edit', compact('dependencia'));
+		return View::make('organizaciones.edit', compact('organizacion'));
 	}
 
 	/**
@@ -104,24 +104,33 @@ class OrganizacionesController extends \BaseController {
 	 */
 	public function update($id)
 	{
-		$dependencia = Dependencia::findOrFail($id);
+		$organizacion = Dependencia::findOrFail($id);
 
         Input::merge(array_map('trim', Input::all()));
 
         $rules = [
             'nombre' => 'required|alpha_num_space|between:1,255',
-            'clave' => 'required|alpha_num_space|between:1,255',
-            'direccion' => 'required|alpha_num_space|between:1,255',
-            'estado' => 'required|in:Activo,Inactivo',
+            'razon_social' => 'required|alpha_space|between:1,255',
+            'direccion' => 'required|digits:5',
+            'codigo_postal' => 'required|alpha_num_space|between:1,255',
+            'contacto' => 'required|alpha_space|between:1,255',
+            'telefono' => 'required|required|regex:/^[0-9]{10,20}$/',
+            'correo' => 'required|email',
+            'estado' => 'required|in:Activo,Inactivo'
         ];
 
         $messages = [
             'required' => 'Este campo es obligatorio.',
             'alpha_num_space' => 'Utilice sólo caracteres del alfabeto, números y espacios.',
+            'alpha_space' => 'Utilice sólo caracteres del alfabeto y espacios.',
+            'codigo_postal.digits' => 'El código postal debe estar formado por 5 caracteres numéricos sin espacios.',
             'estado.integer' => 'Este campo es obligatorio.',
             'between' => 'Este campo es obligatorio.',
-            'in' => 'Este campo es obligatorio.',
+            'telefono.regex' => 'El formato ingresado no es válido',
+            'email' => 'El correo debe estar formado de la siguiente manera: direccion@dominio.com',
+            'in' => 'Este campo es obligatorio.'
         ];
+
 
         $validator = Validator::make($data = Input::all(), $rules, $messages);
 
@@ -130,14 +139,14 @@ class OrganizacionesController extends \BaseController {
 			return Redirect::back()->with('message-type', 'danger')->with('message', 'Algunos datos no han sido propiamente ingresados, favor de revisarlos.')->withErrors($validator)->withInput();
 		}
 
-        $dependencia->update($data);
+        $organizacion->update($data);
 
-		return Redirect::route('dependencias.index')->with('message-type', 'success')
+		return Redirect::route('organizaciones.index')->with('message-type', 'success')
             ->with('message', 'La información se actualizó correctamente.');
 	}
 
 	/**
-	 * Remove the specified dependencia from storage.
+	 * Remove the specified organizacion from storage.
 	 *
 	 * @param  int  $id
 	 * @return Response
@@ -145,8 +154,96 @@ class OrganizacionesController extends \BaseController {
 	public function destroy($id)
 	{
 		Dependencia::destroy($id);
-		return Redirect::route('dependencias.index')->with('message-type', 'success')
+		return Redirect::route('organizaciones.index')->with('message-type', 'success')
 		->with('message', 'El elemento se eliminó correctamente.');
 	}
+
+	    /**
+     * Display a listing of the resource.
+     * GET /organizaciones/search
+     *
+     * @return Response
+     */
+    public function search()
+    {
+
+        $id_organizacion = Input::get('id_organizacion');
+        $nombre = Input::get('nombre');
+        $razon_social = Input::get('razon_social');
+        $direccion = Input::get('direccion');
+        $codigo_postal = Input::get('codigo_postal');
+        $contacto = Input::get('contacto');
+        $telefono = Input::get('telefono');
+        $correo = Input::get('correo');
+        $estado = Input::get('estado');
+        $creacion = Input::get('creacion');
+
+        if(!empty($id_organizacion) )
+            $organizaciones = Organizacion::where('id_organizacion','=',$id_organizacion)->orderBy('id_organizacion', 'desc')->get();
+        else
+        {
+            $query = Organizacion::select();
+            if(!empty($nombre))
+            {
+                $query = $query->where('nombre', 'LIKE', "%{$nombre}%");
+            }
+
+            if(!empty($razon_social))
+            {
+                $query = $query->where('razon_social', 'LIKE', "%{$razon_social}%");
+            }
+
+            if(!empty($direccion))
+            {
+                $query = $query->where('direccion', 'LIKE', "%{$direccion}%");
+            }
+
+            if(!empty($codigo_postal))
+            {
+                $query = $query->where('codigo_postal', '=', "%{$codigo_postal}%");
+            }
+
+            if(!empty($contacto))
+            {
+                $query = $query->where('contacto', 'LIKE', "%{$contacto}%");
+            }
+
+            if(!empty($telefono))
+            {
+                $query = $query->where('direccion', '=', "%{$direccion}%");
+            }
+
+            if(!empty($correo))
+            {
+                $query = $query->where('correo', 'LIKE', "%{$correo}%");
+            }
+
+            if(!empty($estado))
+            {
+                $query = $query->where('estado', '=', $estado);
+            }
+
+            if(!empty($creacion))
+            {
+                $query = $query->where('creacion', '=', $creacion);
+            }
+
+            $organizaciones = $query->orderBy('id_organizacion', 'desc')->get();
+
+        }
+
+        if($organizaciones->isEmpty())
+        {
+            return Redirect::route('organizaciones.index')
+                ->with('message-type', 'warning')
+                ->with('message', 'El criterio de búsqueda no regresó ningún resultado.');
+        }
+        else
+        {
+            return View::make('organizaciones.index', compact('organizaciones'));
+
+        }
+
+    }
 
 }
---
+++ app/views/Organizaciones/create.blade.php
@@ -0,0 +1,115 @@
+@extends('admin.master')
+@section("contenido")
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Organizaciones</h1>
+            @if(Session::has('message'))
+                <div class="alert alert-{{ Session::get('message-type') }} alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Cerrar</span></button>{{ Session::get('message')}}</div>
+            @endif
+            {{  Form::open(array('url' => 'organizaciones', 'class' => 'form-horizontal'))  }}
+                <fieldset>
+                    <!-- Form Name -->
+                    <legend>Nuevo</legend>
+                    <!-- Text input-->
+                    <div class="form-group  has-feedback {{ ($error = $errors->first('nombre')) ? 'has-error' : '' }}">
+                        <label class="col-md-4 control-label" for="nombre">Nombre</label>
+                        <div class="col-md-6">
+                            {{ Form::text('nombre', null, ['class' => 'form-control', 'placeholder' => 'Ingresar nombre de la organización', 'maxlength' => 255] )}}
+                            <span class="help-block">{{ ($error = $errors->first('nombre')) ? $error : '' }}</span>
+                        </div>
+                    </div>
+
+                    <!-- Text input-->
+                    <div class="form-group  has-feedback {{ ($error = $errors->first('razon_social')) ? 'has-error' : '' }}">
+                        <label class="col-md-4 control-label" for="razon_social">Razón Social</label>
+                        <div class="col-md-6">
+                            {{ Form::text('razon_social', null, ['class' => 'form-control', 'placeholder' => 'Ingresar razón social', 'maxlength' => 255] )}}
+                            <span class="help-block">{{ ($error = $errors->first('razon_social')) ? $error : '' }}</span>
+                        </div>
+                    </div>
+
+                    <!-- Text input-->
+                    <div class="form-group  has-feedback {{ ($error = $errors->first('direccion')) ? 'has-error' : '' }}">
+                        <label class="col-md-4 control-label" for="descripcion">Dirección</label>
+                        <div class="col-md-6">
+                            {{ Form::text('direccion', null, ['class' => 'form-control', 'placeholder' => 'Ingresar dirección', 'maxlength' => 255] )}}
+                            <span class="help-block">{{ ($error = $errors->first('direccion')) ? $error : '' }}</span>
+                        </div>
+                    </div>
+
+
+                    <!-- Text input-->
+                    <div class="form-group  has-feedback {{ ($error = $errors->first('codigo_postal')) ? 'has-error' : '' }}">
+                        <label class="col-md-4 control-label" for="codigo_postal">Código Postal</label>
+                        <div class="col-md-6">
+                            {{ Form::text('codigo_postal', null, ['class' => 'form-control', 'placeholder' => 'Ingresar código postal', 'maxlength' => 5] )}}
+                            <span class="help-block">{{ ($error = $errors->first('codigo_postal')) ? $error : '' }}</span>
+                        </div>
+                    </div>
+
+                    <!-- Text input-->
+                    <div class="form-group  has-feedback {{ ($error = $errors->first('contacto')) ? 'has-error' : '' }}">
+                        <label class="col-md-4 control-label" for="contacto">Contacto</label>
+                        <div class="col-md-6">
+                            {{ Form::text('contacto', null, ['class' => 'form-control', 'placeholder' => 'Ingresar nombre del contacto', 'maxlength' => 255] )}}
+                            <span class="help-block">{{ ($error = $errors->first('contacto')) ? $error : '' }}</span>
+                        </div>
+                    </div>
+
+                    <!-- Text input-->
+                    <div class="form-group  has-feedback {{ ($error = $errors->first('telefono')) ? 'has-error' : '' }}">
+                        <label class="col-md-4 control-label" for="telefono">Teléfono</label>
+                        <div class="col-md-6">
+                            {{ Form::text('telefono', null, ['class' => 'form-control', 'placeholder' => 'Ingresar teléfono', 'maxlength' => 10] )}}
+                            <span class="help-block">{{ ($error = $errors->first('telefono')) ? $error : '' }}</span>
+                        </div>
+                    </div>
+
+                    <!-- Text input-->
+                    <div class="form-group  has-feedback {{ ($error = $errors->first('correo')) ? 'has-error' : '' }}">
+                        <label class="col-md-4 control-label" for="correo">Correo</label>
+                        <div class="col-md-6">
+                            {{ Form::text('correo', null, ['class' => 'form-control', 'placeholder' => 'Ingresar correo', 'maxlength' => 255] )}}
+                            <span class="help-block">{{ ($error = $errors->first('correo')) ? $error : '' }}</span>
+                        </div>
+                    </div>
+
+                    <div class="form-group has-feedback {{ ($error = $errors->first('estado')) ? 'has-error' : '' }}">
+                        <label class="col-md-4 control-label" for="estado">Estatus</label>
+                        <div class="col-md-4">
+                            <div class="radio-inline">
+                                <label for="estado">
+                                    {{ Form::radio("estado","Activo",$checked = true)}}
+                                    Activo
+                                </label>
+                            </div>
+                            <div class="radio-inline">
+                                <label>
+                                    {{ Form::radio("estado","Inactivo",$checked = false)}}
+                                    Inactivo
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Button (Double) -->
+                    <div class="form-group">
+                        <label class="col-md-4 control-label" for="guardar"></label>
+                        <div class="col-md-8">
+                            {{ Form::submit('Guardar', array('class' => 'btn btn-primary')) }}
+                            <a href="{{url('organizaciones')}}" class="btn btn-danger"> Salir</a>
+
+                        </div>
+                    </div>
+
+                </fieldset>
+            {{ Form::close() }}
+        </div>
+    </div>
+
+@stop
+@section("js")
+    <script>
+        @include('anos_fiscales.script')
+    </script>
+@stop
